619de9806b5e4d88e228a8d02d8a0c1ec2b8ddd4
daoswap002 <daoswap@daoswap.cc>
2022-04-07 12:19:05 +0800

    init project

---
+++ src/constants/index.js
@@ -0,0 +1,23 @@
+export const StakingInviterForLPTokenAddress1 =
+  "0x69217cF12bD760D6eA850A128646b21F7A61EE07";
+export const StakingInviterForLPContractAddress1 =
+  "0x489cc30482499DA25057B78a9a64043a042BA421";
+
+export const StakingInviterForSingleTokenAddress1 =
+  "0xc096332CAacF00319703558988aD03eC6586e704";
+export const StakingInviterForSingleContractAddress1 =
+  "0xa322bA6c0B8ce79F8EfC3CEc3EB827FD0ED7D84c";
+
+/********** base **********/
+export const DAOAddress = "0xc096332CAacF00319703558988aD03eC6586e704";
+
+export const DSTAddress = "0xC2ed68A614760BFE65e932329199d2F703219B8C";
+
+export const ZeroAddress = "0x0000000000000000000000000000000000000000";
+
+export const CHAIN_ID = process.env.VUE_APP_CHAIN_ID || 56;
+
+export const NETWORK_ID = process.env.VUE_APP_NETWORK_ID || 56;
+
+export const WHITE_LISTS_SWITCH = false;
+export const WHITE_LISTS = ["0x9b1d0c9c1aE96011776e6786b4Efe884665918D2"];
---
+++ src/router/index.js
@@ -0,0 +1,89 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import StakingInviterForLP1 from "../views/staking-inviter/StakingInviterForLP1.vue";
+import StakingInviterForLPHistory1 from "../views/staking-inviter/StakingInviterForLPHistory1.vue";
+// import StakingInviterForSingle1 from "../views/staking-inviter/StakingInviterForSingle1.vue";
+// import StakingInviterForSingleHistory1 from "../views/staking-inviter/StakingInviterForSingleHistory1.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    component: () => import("@/layouts/home/Index.vue"),
+    children: [
+      {
+        path: "",
+        name: "Home",
+        redirect: "/staking",
+        component: () => import("@/views/home/Index.vue")
+      },
+      {
+        path: "/staking",
+        name: "Staking",
+        redirect: "/staking/lp",
+        component: () => import("@/layouts/home/ViewBlank.vue"),
+        children: [
+          {
+            path: "/staking/lp",
+            name: "StakingInviterForLP",
+            redirect: "/staking/lp/1",
+            component: () => import("@/layouts/home/ViewBlank.vue"),
+            children: [
+              {
+                path: "/staking/lp/1",
+                name: "StakingInviterForLP1",
+                component: StakingInviterForLP1
+              },
+              {
+                path: "/staking/lp/1/history",
+                name: "StakingInviterForLP1History",
+                component: StakingInviterForLPHistory1
+              }
+            ]
+          }
+          // {
+          //   path: "/staking/single",
+          //   name: "StakingInviterForSingle",
+          //   redirect: "/staking/single/3",
+          //   component: () => import("@/layouts/home/ViewBlank.vue"),
+          //   children: [
+          //     {
+          //       path: "/staking/single/1",
+          //       name: "StakingInviterForSingle1",
+          //       component: StakingInviterForSingle1
+          //     },
+          //     {
+          //       path: "/staking/single/1/history",
+          //       name: "StakingInviterForSingle1History",
+          //       component: StakingInviterForSingleHistory1
+          //     }
+          //   ]
+          // }
+        ]
+      },
+      {
+        path: "/404",
+        component: () => import("@/views/home/Index.vue")
+      },
+      {
+        path: "*",
+        redirect: "/"
+      }
+    ]
+  }
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  scrollBehavior: (to, from, savedPosition) => {
+    if (to.hash) return { selector: to.hash };
+    if (savedPosition) return savedPosition;
+
+    return { x: 0, y: 0 };
+  },
+  routes
+});
+
+export default router;
